b27a093d0e4e2fd1f2d5cf5a66de38c5a1199b77
Patrick Abrahim <patrick.almeida.abrahim@gmail.com>
2024-01-22 17:04:14 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 22 Jan 2024 19:40:23 GMT
+ */
+
+module.exports = {
+  "ColorGray900": "#121214",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionPurple500": "#8b5cf6ff",
+  "VariableCollectionGray200": "#c1c1c4ff",
+  "VariableCollectionGray500": "#50505cff",
+  "VariableCollectionGray800": "#1c1c1fff"
+};
